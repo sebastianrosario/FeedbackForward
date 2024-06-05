21d5a98fbea4394ea7bf17a95b39cc4ece9e6452
feedbackforwardwit <feedbackforwardwit@gmail.com>
2024-06-05 00:14:46 -0400

    changed auth source

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors');
+var cors = require('cors');
 
 var db = require('./db/db.js');
 
@@ -27,15 +27,12 @@ app.use('/', indexRouter);
 app.use('/api', usersRouter);
 
 app.all('/api', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
  });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
 
@@ -44,10 +41,10 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.log(err);
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+ // res.status(err.status || 500);
+ // res.render('error');
+  res.send(500);
 });
-
 module.exports = app;
