69fb910955d6319a0d830fc6b492b14401f8eeba
Lawrence Scola <scolal@wit.edu>
2024-06-04 22:40:07 -0400

    Moved js from signin.html to new file signin.js

--- FrontEnd/assets/js/signin.js
+++ FrontEnd/assets/js/signin.js
@@ -14,34 +14,14 @@ signinbutton.onclick = function() {
     const hashedPassword = CryptoJS.SHA256(password).toString();
 
     // Create a payload
-    const payload = JSON.stringify({
+    const payload = {
         username: username,
         password: hashedPassword
-    });
+    };
 
-    fetch('http://192.168.28.129:3000/api/users/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: payload
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            alert('Sign in successful');
-            console.log("success");
-            localStorage.setItem('signupPayload', JSON.stringify(payload));
-            window.location.href = "index.html";
-        } else {
-            alert('Sign up failed: ' + data.message);
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('Sign in failed');
-    });
     /***Used for testing the payload contents***/
+    localStorage.setItem('signupPayload', JSON.stringify(payload));
+    window.location.href = "display.html";
 
     /***/
 }
