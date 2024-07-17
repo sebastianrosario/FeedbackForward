ecf2f1dd91e07114be7d13454edac979a063e8e1
karpowicztatwit <karpowiczt@wit.edu>
2024-07-17 19:52:29 -0400

    Work-in-progress Post html and js for post pages.

--- FrontEnd/assets/js/post.js
+++ FrontEnd/assets/js/post.js
@@ -1,14 +1,22 @@
-var currentPost = sessionStorage.getItem(postId)
+//var currentPost = sessionStorage.getItem(postId)
+var currentPostID = sessionStorage.getItem(postId)
+var currentPost = sessionStorage.getItem("66984bd446930466bd080228")
+let currentPostURLStart = "http://192.168.28.129:3000/api/posts/"
+let currentPostIDString = toString(currentPostID)
+let currentPostURL = currentPostURLStart.concat(currentPostIDString);
 
 window.onload = function() {
     /***Used for testing the payload contents***/
-    fetch('http://192.168.28.129:3000/api/posts/currentPost', { // Change to actual variable
+    //fetch('http://192.168.28.129:3000/api/posts/${currentPost}', { // Change to actual variable
+    fetch(currentPostURL, { // Change to actual variable
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
         },
     })
     .then(response => response.json())
+    .then(data => console.log(data.id))
+    /*
     .then(data => {
         if (data.success) {
             alert('Sign up successful, please signin');
@@ -17,8 +25,9 @@ window.onload = function() {
             alert('Sign up failed: ' + data.message);
         }
     })
+    */
     .catch(error => {
         console.error('Error:', error);
-        alert('Sign up failed');
+        alert('Post Fetch Failed');
     });
 }
