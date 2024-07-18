e49941259f5fe7746d2a18f175f7b8dc8ae20002
Lawrence Scola <scolal@wit.edu>
2024-07-18 08:42:43 -0400

    Added username to index.html post

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -10,6 +10,7 @@ window.onload = function () {
         method: "GET",
         headers:{
         'Content-Type': 'application/json',
+        //'Authorization': "Bearer " + sessionStorage.getItem('key')
         'Authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2OGIzOGEyY2E3YjMwNTIxOWJhZDI3ZSIsInVzZXJuYW1lIjoidGVzdHRlc3R0ZXN0IiwiaWF0IjoxNzIxMzAyOTM2LCJleHAiOjE3MjEzMjA5MzZ9.TuvDe4St_wZVrujNAIKe7MXpJFpAShEzsQiGbE-DHAE"
         }
     })
@@ -22,11 +23,13 @@ window.onload = function () {
             const title = message.title;
             const category = message.tags.join(', '); // Combine tags into a single string
             const date = new Date(message.createdAt).toLocaleDateString(); // Format date
+            const user = message.username;
 
             // Update HTML elements with the fetched data
             document.querySelector('.article-category').textContent = category;
             document.querySelector('.article-date').textContent = date;
             document.querySelector('.article-title').textContent = title;
+            document.querySelector('.article-user').textContent = user;
         } else {
             console.error('Failed to fetch article data.');
         }
