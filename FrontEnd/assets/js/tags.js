071a14503b302d493d87b1d4b82d4f0fb67c13c7
Lawrence Scola <scolal@wit.edu>
2024-07-29 01:54:39 -0400

    pushing some additions

--- FrontEnd/assets/js/tags.js
+++ FrontEnd/assets/js/tags.js
@@ -88,3 +88,10 @@ window.onload = function () {
         console.error('Error fetching trending posts:', error);
     });
 }
+
+formOpenBtn.addEventListener('click', () => searchContainer.classList.add('activated'));
+formCloseBtn.addEventListener('click', () => searchContainer.classList.remove('activated'));
+// -- Close the search form popup on ESC keypress
+window.addEventListener('keyup', (event) => {
+    if(event.key === 'Escape') searchContainer.classList.remove('activated');
+});
