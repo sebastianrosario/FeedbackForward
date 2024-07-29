d308120603087140e8f3f00585c8e4bd73537984
Lawrence Scola <scolal@wit.edu>
2024-07-29 10:45:23 -0400

    adding the images to other pages

--- FrontEnd/assets/js/search.js
+++ FrontEnd/assets/js/search.js
@@ -14,72 +14,88 @@ window.onload = function () {
     })
     .then(response => response.json())
     .then(data => {
-        if (data.success) {
-            const posts = data.message;
+        if (posts.length > 0) {
+            const firstItem = posts[0];
+            const secondItem = posts[1];
+            const thirdItem = posts[2];
+            const forthItem = posts[3];
+            const fithItem = posts[4];
 
-            if (posts.length >  0) {
-                const firstItem = posts[0];
-                const secondItem = posts[1];
-                const thirdItem = posts[2];
-                const forthItem = posts[3];
-                const fithItem = posts[4];
+            //first element 
+            const title1 = firstItem.title;
+            const date1 = new Date(firstItem.createdAt).toLocaleDateString(); // Format date
+            const user1 = firstItem.username;
+            const id1 = firstItem._id;
 
-                //first element 
-                const title1 = firstItem.title;
-                const date1 = new Date(firstItem.createdAt).toLocaleDateString(); // Format date
-                const user1 = firstItem.username;
-                const id1 = firstItem._id;
+            const imageElement1 = document.getElementById('trending1-image');
+            const imageURL1 = firstItem.filePath;
+            imageElement1.src = `http://192.168.28.129:3000/files/${imageURL1}`;
 
-                document.getElementById('trending1-date').textContent = date1;
-                document.getElementById('trending1-title').textContent = title1;
-                document.getElementById('trending1-user').textContent = user1;
-                document.getElementById('featuredTrending1-link').href = `http://127.0.0.1:3000/FrontEnd/post.html?id=${id1}`;
+            document.getElementById('trending1-date').textContent = date1;
+            document.getElementById('trending1-title').textContent = title1;
+            document.getElementById('trending1-user').textContent = user1;
+            document.getElementById('featuredTrending1-link').href = `post.html?id=${id1}`;
 
-                //second element
-                
-                const title2 = secondItem.title;
-                const date2 = new Date(secondItem.createdAt).toLocaleDateString(); // Format date
-                const user2 = secondItem.username;
-                const id2 = secondItem._id;
+            //second element
+            
+            const title2 = secondItem.title;
+            const date2 = new Date(secondItem.createdAt).toLocaleDateString(); // Format date
+            const user2 = secondItem.username;
+            const id2 = secondItem._id;
 
-                document.getElementById('trending2-date').textContent = date2;
-                document.getElementById('trending2-title').textContent = title2;
-                document.getElementById('trending2-user').textContent = user2;
-                document.getElementById('featuredTrending2-link').href = `http://127.0.0.1:3000/FrontEnd/post.html?id=${id2}`;
+            const imageElement2 = document.getElementById('trending2-image');
+            const imageURL2 = secondItem.filePath;
+            imageElement2.src = `http://192.168.28.129:3000/files/${imageURL2}`;
 
-                //third element
-                const title3 = thirdItem.title;
-                const date3 = new Date(thirdItem.createdAt).toLocaleDateString(); // Format date
-                const user3 = thirdItem.username;
-                const id3 = thirdItem._id;
+            document.getElementById('trending2-date').textContent = date2;
+            document.getElementById('trending2-title').textContent = title2;
+            document.getElementById('trending2-user').textContent = user2;
+            document.getElementById('featuredTrending2-link').href = `post.html?id=${id2}`;
 
-                document.getElementById('trending3-date').textContent = date3;
-                document.getElementById('trending3-title').textContent = title3;
-                document.getElementById('trending3-user').textContent = user3;
-                document.getElementById('featuredTrending3-link').href = `http://127.0.0.1:3000/FrontEnd/post.html?id=${id3}`;
+            //third element
+            const title3 = thirdItem.title;
+            const date3 = new Date(thirdItem.createdAt).toLocaleDateString(); // Format date
+            const user3 = thirdItem.username;
+            const id3 = thirdItem._id;
 
-                //forth element
-                const title4 = forthItem.title;
-                const date4 = new Date(forthItem.createdAt).toLocaleDateString(); // Format date
-                const user4 = forthItem.username;
-                const id4 = forthItem._id;
+            const imageElement3 = document.getElementById('trending3-image');
+            const imageURL3 = thirdItem.filePath;
+            imageElement3.src = `http://192.168.28.129:3000/files/${imageURL3}`;
 
-                document.getElementById('trending4-date').textContent = date4;
-                document.getElementById('trending4-title').textContent = title4;
-                document.getElementById('trending4-user').textContent = user4;
-                document.getElementById('featuredTrending4-link').href = `http://127.0.0.1:3000/FrontEnd/post.html?id=${id4}`;
+            document.getElementById('trending3-date').textContent = date3;
+            document.getElementById('trending3-title').textContent = title3;
+            document.getElementById('trending3-user').textContent = user3;
+            document.getElementById('featuredTrending3-link').href = `post.html?id=${id3}`;
 
-                //fith
-                const title5 = fithItem.title;
-                const date5 = new Date(fithItem.createdAt).toLocaleDateString(); // Format date
-                const user5 = fithItem.username;
-                const id5 = fithItem._id;
+            //forth element
+            const title4 = forthItem.title;
+            const date4 = new Date(forthItem.createdAt).toLocaleDateString(); // Format date
+            const user4 = forthItem.username;
+            const id4 = forthItem._id;
 
-                document.getElementById('trending5-date').textContent = date5;
-                document.getElementById('trending5-title').textContent = title5;
-                document.getElementById('trending5-user').textContent = user5;
-                document.getElementById('featuredTrending5-link').href = `http://127.0.0.1:3000/FrontEnd/post.html?id=${id5}`;
-            }
+            const imageElement4 = document.getElementById('trending4-image');
+            const imageURL4 = forthItem.filePath;
+            imageElement4.src = `http://192.168.28.129:3000/files/${imageURL4}`;
+
+            document.getElementById('trending4-date').textContent = date4;
+            document.getElementById('trending4-title').textContent = title4;
+            document.getElementById('trending4-user').textContent = user4;
+            document.getElementById('featuredTrending4-link').href = `post.html?id=${id4}`;
+
+            //fith
+            const title5 = fithItem.title;
+            const date5 = new Date(fithItem.createdAt).toLocaleDateString(); // Format date
+            const user5 = fithItem.username;
+            const id5 = fithItem._id;
+
+            const imageElement5 = document.getElementById('trending5-image');
+            const imageURL5 = firstItem.filePath;
+            imageElement5.src = `http://192.168.28.129:3000/files/${imageURL5}`;
+
+            document.getElementById('trending5-date').textContent = date5;
+            document.getElementById('trending5-title').textContent = title5;
+            document.getElementById('trending5-user').textContent = user5;
+            document.getElementById('featuredTrending5-link').href = `post.html?id=${id5}`;
         } else {
             console.error('Failed to fetch trending posts.');
         }
--- FrontEnd/assets/js/user.js
+++ FrontEnd/assets/js/user.js
@@ -38,6 +38,7 @@ window.onload = function() {
     .then(response => response.json())
     .then(data => {
         if (data.success) {
+
             const posts = data.message;
 
             if (posts.length > 0) {
@@ -53,10 +54,14 @@ window.onload = function() {
                 const user1 = firstItem.username;
                 const id1 = firstItem._id;
 
+                const imageElement1 = document.getElementById('trending1-image');
+                const imageURL1 = firstItem.filePath;
+                imageElement1.src = `http://192.168.28.129:3000/files/${imageURL1}`;
+
                 document.getElementById('trending1-date').textContent = date1;
                 document.getElementById('trending1-title').textContent = title1;
                 document.getElementById('trending1-user').textContent = user1;
-                document.getElementById('featuredTrending1-link').href = `http://127.0.0.1:3000/FrontEnd/post.html?id=${id1}`;
+                document.getElementById('featuredTrending1-link').href = `post.html?id=${id1}`;
 
                 //second element
                 
@@ -65,10 +70,14 @@ window.onload = function() {
                 const user2 = secondItem.username;
                 const id2 = secondItem._id;
 
+                const imageElement2 = document.getElementById('trending2-image');
+                const imageURL2 = secondItem.filePath;
+                imageElement2.src = `http://192.168.28.129:3000/files/${imageURL2}`;
+
                 document.getElementById('trending2-date').textContent = date2;
                 document.getElementById('trending2-title').textContent = title2;
                 document.getElementById('trending2-user').textContent = user2;
-                document.getElementById('featuredTrending2-link').href = `http://127.0.0.1:3000/FrontEnd/post.html?id=${id2}`;
+                document.getElementById('featuredTrending2-link').href = `post.html?id=${id2}`;
 
                 //third element
                 const title3 = thirdItem.title;
@@ -76,10 +85,14 @@ window.onload = function() {
                 const user3 = thirdItem.username;
                 const id3 = thirdItem._id;
 
+                const imageElement3 = document.getElementById('trending3-image');
+                const imageURL3 = thirdItem.filePath;
+                imageElement3.src = `http://192.168.28.129:3000/files/${imageURL3}`;
+
                 document.getElementById('trending3-date').textContent = date3;
                 document.getElementById('trending3-title').textContent = title3;
                 document.getElementById('trending3-user').textContent = user3;
-                document.getElementById('featuredTrending3-link').href = `http://127.0.0.1:3000/FrontEnd/post.html?id=${id3}`;
+                document.getElementById('featuredTrending3-link').href = `post.html?id=${id3}`;
 
                 //forth element
                 const title4 = forthItem.title;
@@ -87,10 +100,14 @@ window.onload = function() {
                 const user4 = forthItem.username;
                 const id4 = forthItem._id;
 
+                const imageElement4 = document.getElementById('trending4-image');
+                const imageURL4 = forthItem.filePath;
+                imageElement4.src = `http://192.168.28.129:3000/files/${imageURL4}`;
+
                 document.getElementById('trending4-date').textContent = date4;
                 document.getElementById('trending4-title').textContent = title4;
                 document.getElementById('trending4-user').textContent = user4;
-                document.getElementById('featuredTrending4-link').href = `http://127.0.0.1:3000/FrontEnd/post.html?id=${id4}`;
+                document.getElementById('featuredTrending4-link').href = `post.html?id=${id4}`;
 
                 //fith
                 const title5 = fithItem.title;
@@ -98,11 +115,16 @@ window.onload = function() {
                 const user5 = fithItem.username;
                 const id5 = fithItem._id;
 
+                const imageElement5 = document.getElementById('trending5-image');
+                const imageURL5 = firstItem.filePath;
+                imageElement5.src = `http://192.168.28.129:3000/files/${imageURL5}`;
+
                 document.getElementById('trending5-date').textContent = date5;
                 document.getElementById('trending5-title').textContent = title5;
                 document.getElementById('trending5-user').textContent = user5;
-                document.getElementById('featuredTrending5-link').href = `http://127.0.0.1:3000/FrontEnd/post.html?id=${id5}`;
+                document.getElementById('featuredTrending5-link').href = `post.html?id=${id5}`;
             }
+            
         } else {
             console.error('Failed to fetch trending posts.');
         }
